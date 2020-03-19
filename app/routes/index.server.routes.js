825814e1fc77f57d8f1a257bb4babceb166f0200
Jovane Marques <jovanemarques@gmail.com>
2020-03-19 16:39:12 -0400

    Initial Structure

---
+++ app/routes/index.server.routes.js
@@ -0,0 +1,14 @@
+﻿//This uses CommonJS module pattern to export a single module function.
+//This function takes an express object as argument 
+
+//Load the 'index' controller
+var index = require('../controllers/index.server.controller');
+//
+//handle routing for get and post request
+module.exports = function (app) {
+    //handle a get request made to root path
+    app.get('/', function (req, res) {
+        res.send('OK');
+    }); //go to http://localhost:3000/
+
+};
---
+++ config/env/development.js
@@ -0,0 +1,7 @@
+﻿//Development configuration options
+//To sign the session identifier, use a secret string
+module.exports = {
+    db: 'mongodb://localhost/aesha-jovane-assign3-db',
+    sessionSecret: 'dev-sess-sec-aesha-jovane',
+    secretKey: 'secret-key-aesha-jovane'
+};
